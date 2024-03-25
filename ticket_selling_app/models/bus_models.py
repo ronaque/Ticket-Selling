6a0e4c34101c2c735ab3296a20d2099b0a64ad9d
Isaque <isaque.bm9@gmail.com>
2024-03-24 22:18:43 -0300

    feat: implemented v1.0 and v1.1 models

---
+++ ticket_selling_app/models/bus_models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from ticket_selling_app.models.common import City
+
+
+class BusStation(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+class Bus(models.Model):
+    bus_number = models.CharField(max_length=100)
+
+class BusTravel(models.Model):
+    bus_id = models.ForeignKey(Bus, on_delete=models.CASCADE)
+    bus_station_departure_id = models.ForeignKey(BusStation, on_delete=models.CASCADE)
+    bus_station_arrival_id = models.ForeignKey(BusStation, on_delete=models.CASCADE)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+class BusTicket(models.Model):
+    bus_travel_id = models.ForeignKey(BusTravel, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
