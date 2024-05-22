e8f1232244a00df0858b7be45b83eda7363b45ae
Isaque <isaque.bm9@gmail.com>
2024-05-22 20:36:31 -0300

    feat: implemented tuple with travel and city on search-path endpoint

--- TicketSelling/urls.py
+++ TicketSelling/urls.py
@@ -60,4 +60,5 @@ urlpatterns = [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path("search-path/", views.path_between_cities, name="search-path"),
 ]
